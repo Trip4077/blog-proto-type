1bf1dd93998361f294ccafd16db0f10f7be98fae
Trip4077 <bj2336@email.vccs.edu>
2020-04-07 15:17:23 -0400

    Landing Newsletter Banner Registration Form Connection

--- src/components/newsletter/NewsLetterForm.jsx
+++ src/components/newsletter/NewsLetterForm.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const NewsLetterForm = () => {
-    return(
-        <form name="newsletter">
-            <div className="input-label">
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required />
-            </div>
-            <div className="input-label">
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
-            </div>
+    const [ formData, setFormData ] = useState( { email: null, password: null } );
+    const [ message, setMessage ] = useState( '' )
 
-            <button>Sign Up!</button>
-        </form>
+    const changeHandler = e => {
+        e.preventDefault();
+
+        const field = e.target.name;
+        const value = e.target.value;
+
+        const formInfo = { ...formData }
+
+        formInfo[field] = value
+
+        setFormData( formInfo )
+    }
+
+    const registerUser = e => {
+        e.preventDefault();
+
+        axios.post( 'http://localhost:5000/api/auth/register', formData )
+             .then( res => setMessage( "Registration Successful!" ) )
+             .catch( err => setMessage( err.response.data.message ) );
+    }
+   
+    return(
+        <>
+            <p>{message}</p>
+            <form name="newsletter" onSubmit={ registerUser } >
+                <div className="input-label">
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" name="email" onChange={changeHandler} required />
+                </div>
+                <div className="input-label">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" onChange={changeHandler} required />
+                </div>
+
+                <button type='submit'>Sign Up!</button>
+            </form>
+        </>
     )   
 }
 
